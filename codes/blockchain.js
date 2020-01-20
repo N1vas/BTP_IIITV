0654ec5ae6f8ec565cb894d3a2699e8f782e16f3
N1vas <ggj.nivas.18@gmail.com>
2020-01-20 11:52:43 +0530

    created new files

--- codes/blockchain.js
+++ codes/blockchain.js
@@ -1,5 +1,52 @@
+var crypto = require('crypto');
 
 
 
+class Block{
 
-class Block{}
+
+    constructor(index,timestamp,data,previousHash=""){
+        this.index = index;
+        this.timestamp = new Date().toString();
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+       
+        this.nonce = 0;
+       
+    }
+
+   calculateHash() {
+    return crypto.createHash('sha256').update(this.data+this.previousHash).digest('hex');
+
+   }
+
+    
+}
+
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesis()];
+    }
+
+    createGenesis() {
+        return new Block(0, new Date().toString() , "Genesis block", "0");
+    }
+
+    latestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.latestBlock().hash;
+        this.chain.push(newBlock);
+    }
+}
+
+var testChain = new Blockchain();
+
+testChain.addBlock(new Block(1, new Date().toString(), "This is block 1"));
+
+testChain.addBlock(new Block(2,new Date().toString(), "This is block 2"));
+console.log(testChain);
